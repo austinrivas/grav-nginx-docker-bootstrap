eb9d3a7567047f523ed6fc2a2bf4c5ecb54e100b
Austin Rivas <arivas@pushhere.com>
2017-12-08 17:11:13 -0500

    added featured project data and progressive enhancement

--- grav/user/themes/haywire/js/components/FeaturedProject.vue
+++ grav/user/themes/haywire/js/components/FeaturedProject.vue
@@ -7,17 +7,57 @@
         computed: {
             id: function () {
                 return this.model && this.model.id ? this.model.id : null;
+            },
+            title: function () {
+
+                let title = [];
+
+                if (this.model) {
+                    if (this.model.address) {
+                        title.push(this.model.address)
+                    }
+
+                    if (this.model.lotId) {
+                        title.push(`LOT ${this.model.lotId}`);
+                    }
+                }
+
+                return this.model && title.length ? title.join(' - ') : false;
+            },
+            price: function () {
+                return this.model && this.model.totalPrice ? this.model.totalPrice : false;
+            },
+            image: function () {
+                return this.model ? this.model.imageUrl : false;
+            },
+            subtitle: function () {
+                return this.model ? this.model.subdivision : false;
+            },
+            details: function () {
+
+                let details = [];
+
+                if (this.model) {
+
+                    if (this.model.acres) {
+                        details.push(`${this.model.acres} ACRES`);
+                    }
+
+                    if (this.model.type) {
+                        details.push(`${this.model.type}`)
+                    }
+
+                    if (this.model.status) {
+                        details.push(`${this.model.status}`);
+                    }
+
+                    if (this.model.pricePerSqft) {
+                        details.push(`${this.model.pricePerSqft} PSF`);
+                    }
+                }
+
+                return this.model && details.length ? details.join(' | ') : false;
             }
-//            ,
-//            propImg: function () {
-//                return this.model ? this.model.imgUrl : null;
-//            },
-//            subtitle: function () {
-//                return this.model ? "subtitle" : null;
-//            },
-//            propDetails: function () {
-//                return this.model ? "details" : null;
-//            }
         }
     }
 </script>
