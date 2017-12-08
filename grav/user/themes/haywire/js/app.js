3b29dc8aa134ed1e9835f56e0e307e90659085a6
Austin Rivas <arivas@pushhere.com>
2017-12-08 17:10:39 -0500

    added vue currency life

--- grav/user/themes/haywire/js/app.js
+++ grav/user/themes/haywire/js/app.js
@@ -6,6 +6,7 @@ import "babel-polyfill";
 import ArcModel from "./models/arcModel";
 import PropertyCollection from "./models/propertyCollection"
 import FavoritePropertiesCollection from "./models/favoritePropertiesCollection"
+import VueCurrencyFilter from "./filters/vueCurrencyFilter";
 
 /**
  * Load all of this project's JavaScript dependencies including Vue
@@ -27,6 +28,15 @@ Vue.component('favorite-icon', require('./components/FavoriteIcon.vue'));
 Vue.component('featured-project', require('./components/FeaturedProject.vue'));
 Vue.component('featured-projects', require('./components/FeaturedProjects.vue'));
 Vue.component('navbar', require('./components/Navbar.vue'));
+
+Vue.use(VueCurrencyFilter, {
+    symbol : '$',
+    thousandsSeparator: ',',
+    fractionCount: 0,
+    fractionSeparator: '.',
+    symbolPosition: 'front',
+    symbolSpacing: true
+});
 
 const app = new Vue({
     delimiters: ['${', '}'],
