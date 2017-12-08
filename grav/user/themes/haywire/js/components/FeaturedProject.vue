99c6ff16a5d6af1edd9835ae5ccf1fa21e6585fd
Austin Rivas <arivas@pushhere.com>
2017-12-08 16:15:32 -0500

    featured projects component loads arc data and passes it to child components

--- grav/user/themes/haywire/js/components/FeaturedProject.vue
+++ grav/user/themes/haywire/js/components/FeaturedProject.vue
@@ -1,13 +1,23 @@
 <script>
+    import PropertyModel from "../models/propertyModel";
+
     export default {
-        mounted() {
-            //
-        },
+        props: ['model'],
 
-        data() {
-            return {}
-        },
-
-        methods: {}
+        computed: {
+            id: function () {
+                return this.model && this.model.id ? this.model.id : null;
+            }
+//            ,
+//            propImg: function () {
+//                return this.model ? this.model.imgUrl : null;
+//            },
+//            subtitle: function () {
+//                return this.model ? "subtitle" : null;
+//            },
+//            propDetails: function () {
+//                return this.model ? "details" : null;
+//            }
+        }
     }
 </script>
