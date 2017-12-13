852a3a1e4e629f15bbaf444a1ff2af44a3bde62c
Austin Rivas <arivas@pushhere.com>
2017-12-13 16:45:40 -0500

    added map component event bindings that watch properties property of arcmap

--- grav/user/themes/haywire/js/components/ArcMap.vue
+++ grav/user/themes/haywire/js/components/ArcMap.vue
@@ -10,14 +10,15 @@
 
             const basemap = "hybrid",
                 options = {},
-                mouseWheelEvent = "mouse-wheel",
                 modules = [
+                    "esri/geometry/Extent",
+                    "esri/layers/FeatureLayer",
+                    "esri/Graphic",
                     "esri/Map",
                     "esri/views/MapView",
-                    "esri/layers/FeatureLayer",
-                    "esri/symbols/SimpleMarkerSymbol",
                     "esri/geometry/Point",
-                    "esri/Graphic"];
+                    "esri/symbols/SimpleMarkerSymbol"
+                ];
 
             _this.mapNodeSelector = "mapNode";
 
@@ -26,8 +27,9 @@
             });
 
             await esriLoader.loadModules(modules, options)
-                .then(([Map, MapView, FeatureLayer, SimpleMarkerSymbol, Point, Graphic]) => {
+                .then(([Extent, FeatureLayer, Graphic, Map, MapView, Point, SimpleMarkerSymbol]) => {
 
+                    _this.Extent = Extent;
                     _this.FeatureLayer = FeatureLayer;
                     _this.Graphic = Graphic;
                     _this.Map = Map;
@@ -47,74 +49,95 @@
 
             _this.properties = await Properties.findAllProperties();
 
-            _this.mapView = new _this.MapView({
-                container: _this.mapNodeSelector,
-                map: _this.map,
-                zoom: _this.defaultZoom,
-                center: await _this.getMapCenter(_this.properties) // [longitude, latitude]
-            });
-
-            _this.mapView.on(mouseWheelEvent, _this.stopPropagation);
-
-            _this.properties.forEach(_this.addMapMarkerToDisplay);
         },
 
         data: function () {
             return {
+                Extent: null,
                 FeatureLayer: null,
                 Graphic: null,
                 Map: null,
                 MapView: null,
                 Point: null,
                 SimpleMarkerSymbol: null,
-                defaultZoom: 16,
+                extent: null,
                 featureLayer: null,
                 featureServer: {url: null},
                 graphics: [],
                 map: null,
                 mapNodeSelector: null,
                 mapView: null,
-                properties: null,
+                markerColor: [200, 25, 25, 0.8],
+                markerOutlineWidth: "0.5px",
+                markerPath: "M16,3.5c-4.142,0-7.5,3.358-7.5,7.5c0,4.143,7.5,18.121,7.5,18.121S23.5,15.143,23.5,11C23.5,6.858,20.143,3.5,16,3.5z M16,14.584c-1.979,0-3.584-1.604-3.584-3.584S14.021,7.416,16,7.416S19.584,9.021,19.584,11S17.979,14.584,16,14.584z",
+                markerSize: "24px",
+                properties: null
             };
         },
 
-        computed: {},
+        watch: {
+            properties: async function () {
+                const _this = this,
+                    mouseWheelEvent = "mouse-wheel";
+
+                _this.extent = {
+                    xmin: null,
+                    ymin: null,
+                    xmax: null,
+                    ymax: null
+                };
+
+                _this.removeAllMarkers();
+
+                _this.mapView = new _this.MapView({
+                    container: _this.mapNodeSelector,
+                    map: _this.map,
+                    center: await _this.getMapCenter(_this.properties)
+                });
+
+                _this.mapView.on(mouseWheelEvent, _this.stopPropagation);
+
+                await _this.properties.forEach(_this.addPropertyToMap);
+
+                _this.mapView.extent = _this.extent;
+            }
+        },
 
         methods: {
-            addMapMarkerToDisplay: function (model) {
-                const markerPath = "M16,3.5c-4.142,0-7.5,3.358-7.5,7.5c0,4.143,7.5,18.121,7.5,18.121S23.5,15.143,23.5,11C23.5,6.858,20.143,3.5,16,3.5z M16,14.584c-1.979,0-3.584-1.604-3.584-3.584S14.021,7.416,16,7.416S19.584,9.021,19.584,11S17.979,14.584,16,14.584z",
-                    markerSize = "24px",
-                    markerColor = [200, 25, 25, 0.8],
-                    markerOutlineWidth = "0.5px";
-
+            addPropertyToMap: function (model) {
                 let _this = this,
-                    pnt = new _this.Point({
+                    gr = _this.createMarker(model);
+
+                _this.extent = _this.updateMapExtent(model, _this.extent);
+
+                _this.graphics.push({ graphic: gr });
+                _this.mapView.graphics.add(gr);
+            },
+            createMarker: function (model) {
+                let _this = this;
+                return new _this.Graphic({
+                    geometry: new _this.Point({
                         longitude: model.centroid.longitude,
                         latitude: model.centroid.latitude
                     }),
-                    gr = new _this.Graphic({
-                        geometry: pnt,
-                        symbol: new _this.SimpleMarkerSymbol({
-                            path: markerPath,
-                            size: markerSize,
-                            color: markerColor,
-                            outline: {
-                                color: markerColor,
-                                width: markerOutlineWidth
-                            }
-                        }),
-                        layer: _this.featureLayer,
-                        popupTemplate: {
-                            title: model.address,
-                            content: _this.createMarkerContent(model)
+                    symbol: new _this.SimpleMarkerSymbol({
+                        path: _this.markerPath,
+                        size: _this.markerSize,
+                        color: _this.markerColor,
+                        outline: {
+                            color: _this.markerColor,
+                            width: _this.markerOutlineWidth
                         }
-                    });
-
-                _this.graphics.push({ graphic: gr });
-                _this.mapView.graphics.add(gr)
+                    }),
+                    layer: _this.featureLayer,
+                    popupTemplate: {
+                        title: model.address,
+                        content: _this.createMarkerContent(model)
+                    }
+                })
             },
             createMarkerContent: function (model) {
-                return `<p>${model.toJSON()}</p>`;
+                return `<p>${model.address}</p>`;
             },
             getMapCenter: async function (collection) {
                 const center = await collection.reduce((accumulator, model) => {
@@ -135,6 +158,25 @@
                 if (e && e.stopPropagation) {
                     e.stopPropagation();
                 }
+            },
+            updateMapExtent: function (model, extent) {
+                if (!extent.xmin || model.centroid.longitude < extent.xmin) {
+                    extent.xmin = model.centroid.longitude;
+                }
+
+                if (!extent.xmax || model.centroid.longitude > extent.xmax) {
+                    extent.xmax = model.centroid.longitude;
+                }
+
+                if (!extent.ymin || model.centroid.latitude < extent.ymin) {
+                    extent.ymin = model.centroid.latitude;
+                }
+
+                if (!extent.ymax || model.centroid.latitude > extent.ymax) {
+                    extent.ymax = model.centroid.latitude;
+                }
+
+                return extent;
             }
         }
     }
