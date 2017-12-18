e957cd42a4a18c638ba4f7b00d50a5618f9e9ea4
Austin Rivas <arivas@pushhere.com>
2017-12-18 11:48:18 -0500

    select range and property tile comments

--- grav/user/themes/haywire/js/components/RangeSlider.vue
+++ grav/user/themes/haywire/js/components/RangeSlider.vue
@@ -1,43 +1,47 @@
 <script>
+    // a generic component for a range slider that emits change events on a shared event bus
     export default {
         props: [
-            'changeEvent',
-            'maxValue',
-            'minValue',
-            'outputFunction',
-            'parentEventBus',
-            'step',
-            'values'
+            'changeEvent', // named change event
+            'maxValue', // max value of range slider
+            'minValue', // min value of the range slider
+            'outputFunction', // function for generating the output label of the range slider
+            'parentEventBus', // shared event bus
+            'step', // range step value
+            'values' // selected values as defined by the parent component
         ],
 
+        // runs when component is declared in memory
         created() {
             let _this = this;
-
+            // if outputFunction is defined and a function, set it as the outputFactory
             if (_this.outputFunction && typeof _this.outputFunction === 'function') {
                 _this.outputFactory = _this.outputFunction;
             }
-
+            // duck type parentEventBus prior to application
             if (_this.parentEventBus && _this.parentEventBus.$on && _this.parentEventBus.$emit) {
                 _this.eventBus = _this.parentEventBus;
             }
         },
 
+        // runs when component is attached to the DOM
         async mounted() {
             let _this = this;
-
-            _this.sliderStep = _this.step || 1;
+            // default slider step
+            _this.sliderStep = _this.step ? _this.step : _this.sliderStep;
             _this.minSliderValue = _this.minValue;
             _this.maxSliderValue = _this.maxValue;
+            // if values are defined use them, else use min / max values for initial selected state
             _this.valueSliderA = _this.values && _this.values.length === 2 ? _this.values[0] : _this.minSliderValue;
             _this.valueSliderB = _this.values && _this.values.length === 2 ? _this.values[1] : _this.maxSliderValue;
+            // order the selected slider values [ min, max ]
             _this.sliderValues = await _this.getSliderValues([_this.valueSliderA, _this.valueSliderB]);
         },
 
         data() {
             let _this = this;
-
             return {
-                eventBus: {
+                eventBus: { // mock event bus
                     $on() {
                         console.log('No parent event bus defined', _this.parentEventBus);
                     },
@@ -47,15 +51,16 @@
                 },
                 minSliderValue: null,
                 maxSliderValue: null,
-                outputFactory(values) { console.log('No output function for range slider', values) },
-                sliderStep: null,
-                sliderValues: [],
+                outputFactory(values) { console.log('No output function for range slider', values) }, // mock outputFactory
+                sliderStep: 1,
+                sliderValues: [], // [ min, max ]
                 valueSliderA: null,
                 valueSliderB: null
             }
         },
 
         computed: {
+            // computed prop that returns the result of running the outputFactory method with the current slider values
             sliderOutput() {
                 let _this = this;
                 return _this.outputFactory(_this.sliderValues);
@@ -63,68 +68,66 @@
         },
 
         watch: {
+            // watch the maxValue prop
             maxValue() {
                 let _this = this;
-
+                // set the maxSliderValue to the maxValue prop
                 _this.maxSliderValue = _this.maxValue;
-
-                if (!_this.valueSliderB) {
-                    _this.valueSliderB = _this.maxValue;
-                }
+                // if the value for sliderB is undefined set it to max value
+                if (!_this.valueSliderB) { _this.valueSliderB = _this.maxValue; }
             },
+            // watch the minValue prop
             minValue() {
                 let _this = this;
-
+                // set the minSliderValue to the maxValue prop
                 _this.minSliderValue = _this.minValue;
-
-                if (!_this.valueSliderA) {
-                    _this.valueSliderA = _this.minValue;
-                }
+                // if the value for sliderA is undefined set it to min value
+                if (!_this.valueSliderA) { _this.valueSliderA = _this.minValue; }
             },
-            async values() {
+            // watch the values prop
+            values() {
                 let _this = this;
-
+                // when values changes set the valueSliderA, valueSliderB props accordingly
                 if (_this.values && _this.values.length === 2) {
                     _this.valueSliderA = _this.values[0];
                     _this.valueSliderB = _this.values[1];
                 }
             },
+            // watch the valueSliderA prop
             async valueSliderA() {
                 let _this = this;
-
                 _this.sliderValues = await _this.getSliderValues();
             },
+            // watch the valueSliderB prop
             async valueSliderB() {
                 let _this = this;
-
                 _this.sliderValues = await _this.getSliderValues();
             }
         },
 
         methods: {
+            // parse the values for slider A and slider B into an array and then do a bitwise array swap to order the values in [ min, max ] order
             async getSliderValues() {
                 let _this = this,
                     array = await [_this.valueSliderA, _this.valueSliderB].reduce((accumulator, value) => {
                         accumulator.push(JSON.parse(value));
                         return accumulator;
                     }, []);
-
                 return _this.bitwiseArraySwap(array);
-
-
             },
+            // order the 2 elements of a [ number, number ] array in [ min, max ] order
             bitwiseArraySwap(array) {
                 if (array && array.length === 2) {
                     if (array[0] > array[1]) {
                         [array[1], array[0]] = [array[0], array[1]]
                     }
                 }
-
                 return array;
             },
+            // change handler for slider values
             handleChange(e) {
                 let _this = this;
-
+                // if changeEvent is defined emit a change event on the shared event bus using the current values of the slider
                 if (_this.changeEvent && _this.changeEvent.length) {
                     _this.eventBus.$emit(_this.changeEvent, _this.sliderValues);
                 }
--- grav/user/themes/haywire/js/components/SelectFilter.vue
+++ grav/user/themes/haywire/js/components/SelectFilter.vue
@@ -1,31 +1,34 @@
 <script>
+    // generic select component that emits change events on a shared event bus
     export default {
         props: [
-            'changeEvent',
-            'options',
-            'parentEventBus',
-            'selected'
+            'changeEvent', // name change event
+            'options', // selectable options
+            'parentEventBus', // shared event bus
+            'selected' // current selection as defined by parent component
         ],
 
+        // runs when component is declared in memory
         created() {
             let _this = this;
-
+            // duck type parent event bus prop prior to application
             if (_this.parentEventBus && _this.parentEventBus.$on && _this.parentEventBus.$emit) {
                 _this.eventBus = _this.parentEventBus;
             }
         },
 
+        // runs when component is attached to the DOM
         mounted() {
             let _this = this;
-
+            // set the currently selected option to the value of the selected prop
             _this.selectedOption = _this.selected;
         },
 
+
         data() {
             let _this = this;
-
             return {
-                eventBus: {
+                eventBus: { // mock event bus
                     $on() {
                         console.log('No parent event bus defined', _this.parentEventBus);
                     },
@@ -33,21 +36,24 @@
                         console.log('No parent event bus defined', _this.parentEventBus);
                     }
                 },
-                selectedOption: null
+                selectedOption: null // default selected option
             }
         },
 
         watch: {
+            // watch the value of the selected prop for changes
             selected() {
                 let _this = this;
+                // set the selectedOption to the new value of selected
                 _this.selectedOption = _this.selected;
             }
         },
 
         methods: {
+            // event handler for selections
             handleSelectChange(e) {
                 let _this = this;
-
+                // if changeEvent is defined emit a change event using the current selected option as the value
                 if (_this.changeEvent && _this.changeEvent.length) {
                     _this.eventBus.$emit(_this.changeEvent, _this.selectedOption);
                 }
