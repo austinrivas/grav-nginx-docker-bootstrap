3dd39db9815924d72102f7aed43b585aa4d12c34
Oleg Nazarov <onazarov@pushhere.com>
2018-01-02 14:46:22 -0500

    touched single property view

--- grav/user/themes/haywire/js/components/PropertyInfoTable.vue
+++ grav/user/themes/haywire/js/components/PropertyInfoTable.vue
@@ -13,10 +13,17 @@
             rows() {
                 let _this = this;
                 return _this.createRows(_this.columns, _this.models);
+            },
+            mobileRows() {
+                let _this = this;
+                return _this.createMobileRows(_this.columns, _this.models);
             }
         },
 
         methods: {
+            applyColumnFilter(filter, value) {
+                return filter && typeof filter === 'function' ? filter(value) : value;
+            },
             createHeaders(columns) {
                 if (columns && columns.length) {
                     return columns.reduce((accumulator, column) => {
@@ -30,23 +37,12 @@
                 }
             },
             createRows(columns, models) {
-                let getColumnValue = function (column, model) {
-                    let computed = column.computed,
-                        key = column.key;
-                    if (key) {
-                        return model[key];
-                    } else if (computed && typeof computed === 'function') {
-                        return computed(model);
-                    } else {
-                        return null;
-                    }
-                };
+                let _this = this;
                 if (models && models.length && columns && columns.length) {
                     return models.reduce((rows, model) => {
                         let row = columns.reduce((row, column) => {
-                            let value = getColumnValue(column, model),
-                                filter = column.filter;
-                            row.push(filter && typeof filter === 'function' ? filter(value) : value);
+                            let value = _this.getColumnValue(column, model)
+                            row.push( _this.applyColumnFilter(column.filter, value) );
                             return row;
                         }, []);
                         rows.push(row);
@@ -54,6 +50,37 @@
                     }, []);
                 } else {
                     return [];
+                }
+            },
+            createMobileRows(columns, models) {
+                let _this = this,
+                    model = models && models.length ? models[0] : null;
+                if (model && columns && columns.length) {
+                    return columns.reduce((accumulator, column) => {
+                        accumulator.push({
+                            value: column.heading,
+                            class: 'header'
+                        });
+                        let value = _this.getColumnValue(column, model)
+                        accumulator.push({
+                            value: _this.applyColumnFilter(column.filter, value),
+                            class: 'cell'
+                        });
+                        return accumulator;
+                    }, []);
+                } else {
+                    return [];
+                }
+            },
+            getColumnValue(column, model) {
+                let computed = column.computed,
+                    key = column.key;
+                if (key) {
+                    return model[key];
+                    } else if (computed && typeof computed === 'function') {
+                        return computed(model);
+                    } else {
+                    return null;
                 }
             }
         }
