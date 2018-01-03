ddb09ecd5e5326c8ca920543ff9b1054e70c879b
Austin Rivas <arivas@pushhere.com>
2018-01-03 14:59:06 -0500

    added progress bar component and styles

---
+++ grav/user/themes/haywire/js/components/ProgressBar.vue
@@ -0,0 +1,31 @@
+<script>
+    // NProgress Bar GH Page
+    // https://github.com/rstacruz/nprogress
+    import NProgress from '../vendor/nProgress';
+    export default {
+        template: `<div id="progress-bar"></div>`,
+        created() {
+            window.addEventListener('load', () => {
+                if (NProgress && NProgress.done) {
+                    // complete progress bar loading animation
+                    NProgress.done();
+                }
+            });
+        },
+        mounted() {
+            let _this = this;
+            if (NProgress && NProgress.configure && NProgress.start) {
+                NProgress.configure(_this.config);
+                NProgress.start();
+            }
+        },
+        data() {
+            return {
+                config: {
+                    parent: '#progress-bar',
+                    showSpinner: false
+                }
+            };
+        }
+    }
+</script>
