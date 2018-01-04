8fa0a14c71bed7640f6dab3f840c459e31ac4291
Austin Rivas <arivas@pushhere.com>
2018-01-04 17:36:26 -0500

    added option to force deploy and overwrite env

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,26 +1,20 @@
 #!/usr/bin/env bash
 
-# deploys the application to the specified environment
-function push_git_deploy() {
-	git push $1 +HEAD:master
-	git fetch $1
-	git push origin --tags
-}
-
-# https://superuser.com/questions/547282/which-is-the-rsync-command-to-smartly-merge-two-folders#547316
-# merges the local pages directory with the source directory
-merge_env_pages() {
-	rsync -avc push_dev@lc-www1:~/www.lwrcommercial.com/$1/grav/user/pages/ ./grav/user/pages
-}
+source ./git_deploy.sh
+source ./merge_env_pages.sh
 
 merge_env_pages $1
 
 if git diff-index --quiet HEAD --; then
-    # No changes to pages directory
-    # push_git_deploy $1
-    echo "No changes detected"
+    git_deploy $1
 else
     # Changes to pages directory
     echo "The ./grav/user/pages directory on $1 has changes that have not been committed to this deployment."
-    echo "Commit the changes and deploy again. You can force the deploy with the --force option."
+
+    read -p "Would you like to overwrite the changes made on $1? [Y/n]" -n 1 -r
+    if [[ ! $REPLY =~ ^[Yy]$ ]]
+        git_deploy $1
+    then
+        [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+    fi
 fi
