c12f5a004e98e098570815dcf3e4630d5c526576
Austin Rivas <arivas@pushhere.com>
2018-01-05 18:03:29 -0500

    updated build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,45 +1,90 @@
 #!/bin/bash
-
-# Assign passed in variables
-# FILES_CHANGED is a comma seperated list of files that changed in this deployment
-FILES_CHANGED=$1
-APP_ENV=$2
-
-echo "------------ Application Environment ---------------"
-echo $APP_ENV
-echo "----------------------------------------------------"
-echo "------------ Changed the following files... --------"
-echo $FILES_CHANGED
-echo "----------------------------------------------------"
-
 # Ensure we get our whole bash environment
 source $HOME/.bash_profile
+# FILES_CHANGED is a comma seperated list of files that changed in this deployment
+FILES_CHANGED=$1
+# test | stage | prod
+APP_ENV=$2
+APP_ROOT=$(pwd)
+GRAV_ROOT=$APP_ROOT/grav
+THEME_NAME=haywire
+THEME_ROOT=$GRAV_ROOT/user/themes/$THEME_NAME
 
-echo "Starting the build process..."
+function log_header() {
+    LINE_BREAK='#===================================================='
+    HEADER=$1
+    printf '%s\n# %s\n%s\n' $LINE_BREAK "$HEADER" $LINE_BREAK
+}
 
-echo "=================== NPM Version ===================="
-npm -v
+function build_application() {
+    log_header 'Start Building Application'
+    create_backup
+    install_dependencies
+    clear_cache
+    build_front_end
+    log_header 'Finished Building Application'
+}
 
-echo "=================== Node Version ==================="
-node -v
+function build_front_end() {
+    log_header 'Start Building Front-End'
+    cd $THEME_ROOT
+    yarn install
+    yarn && yarn run production
+    log_header 'Finished Building Front-End'
+}
 
-echo "================= Composer Version ================="
-composer --version
+function clear_cache() {
+    log_header 'Start Clearing Cache'
+    cd $GRAV_ROOT
+    bin/grav clear-cache --all
+    log_header 'Finished Clearing Cache'
+}
 
-echo "=================== Dependencies ==================="
-cd grav/
-bin/grav install
-composer install
+function create_backup() {
+    log_header 'Start Creating Backup'
+    cd $GRAV_ROOT/backup
+    rm ./backup.zip.previous
+    find . -name 'backup.zip.current' -exec bash -c 'mv "$1" "${1%/*}"/backup.zip.previous' -- {} \;
+    cd $GRAV_ROOT
+    bin/grav backup
+    cd $GRAV_ROOT/backup
+    find . -name '*.zip' -exec bash -c 'mv "$1" "${1%/*}"/backup.zip.current' -- {} \;
+    log_header 'Finished Creating Backup'
+}
 
-echo "====================== Build ======================="
-cd user/themes/haywire
-yarn install
-yarn && yarn run production
+function install_dependencies() {
+    log_header 'Start Installing Dependencies'
+    cd $GRAV_ROOT
+    bin/grav install
+    composer install
+    log_header 'Finished Installing Dependencies'
+}
+
+function log_configuration() {
+    log_header 'Environment Name : '$APP_ENV
+    log_header 'Node Version '$(node -v)
+    log_header 'NPM Version '$(npm -v)
+    log_header "$(composer --version --no-ansi)"
+}
+
+function log_changed_files() {
+    log_header 'Changed Files'
+    printf "%s\n" $FILES_CHANGED
+    log_header 'END of Changed Files'
+}
 
 # Check to see if our nginx vhost files have changed
 # If so then we need to restart our Nginx service
-ENV_VHOST="nginx/sites-available/vhost-$APP_ENV.conf"
-if [[ $FILES_CHANGED == *"$ENV_VHOST"* ]]; then
-  echo "Virtual host file was changed for this environment. Bouncing web server..."
-  sudo /bin/systemctl restart nginx
-fi
+function restart_nginx() {
+    ENV_VHOST="nginx/sites-available/vhost-$APP_ENV.conf"
+    if [[ $FILES_CHANGED == *"$ENV_VHOST"* ]]; then
+      log_header 'Restarting NGINX'
+      sudo /bin/systemctl restart nginxcomp
+      log_header 'NGINX Restarted'
+    fi
+}
+
+log_configuration
+log_changed_files
+build_application
+restart_nginx
