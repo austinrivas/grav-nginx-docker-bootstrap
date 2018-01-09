83359dde6e7da9539bc7a0dccc3fb4e061a7e5f0
Austin Rivas <arivas@pushhere.com>
2018-01-09 16:07:51 -0500

    fixed rsync paths

--- scripts/merge_env.sh
+++ scripts/merge_env.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 # https://superuser.com/questions/547282/which-is-the-rsync-command-to-smartly-merge-two-folders#547316
 # merges the local pages directory with the source directory
-REMOTE_PATH=push_dev@lc-www1:~/www.lwrcommercial.com
-GRAV_PAGES_PATH=grav/user/pages
-GRAV_CONFIG_PATH=grav/user/config
+REMOTE_PATH='push_dev@lc-www1:~/www.lwrcommercial.com'
+GRAV_USER_PATH='grav/user'
+GRAV_PAGES_PATH="$GRAV_USER_PATH/pages"
+GRAV_CONFIG_PATH="$GRAV_USER_PATH/config"
 merge_env_pages() {
-	rsync -aviuzP $REMOTE_PATH/$1/$GRAV_PAGES_PATH ./$GRAV_PAGES_PATH
+	rsync -aviuzP "$REMOTE_PATH/$1/$GRAV_PAGES_PATH" "./$GRAV_USER_PATH"
 }
 
 merge_env_config() {
-    rsync -aviuzP $REMOTE_PATH/$1/$GRAV_CONFIG_PATH ./$GRAV_CONFIG_PATH
+    rsync -aviuzP "$REMOTE_PATH/$1/$GRAV_CONFIG_PATH" "./$GRAV_USER_PATH"
 }
