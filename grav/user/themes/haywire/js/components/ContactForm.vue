136572c5912828300960b20f9a09e7348c82e259
Justin Workman <jworkman@Push-Computer-79.local>
2018-01-09 16:33:15 -0500

    Adding components that didn't exist

---
+++ grav/user/themes/haywire/js/components/ContactForm.vue
@@ -0,0 +1,35 @@
+<script>
+
+var VueScrollTo = require('vue-scrollto');
+
+    export default {
+    	mounted(){
+    		if ( typeof(this.$refs.notice) !== "undefined" && this.$refs.notice ) {
+    			this.scrollDown()
+    		}
+    	},
+        methods: {
+            scrollDown() {
+
+            	const height = this.$refs.notice.offsetHeight
+
+            	var options = {
+				    container: 'body',
+				    easing: 'ease-in',
+				    offset: (height - 80),
+				    cancelable: true,
+				    onDone: function() {
+				      // scrolling is done
+				    },
+				    onCancel: function() {
+				      // scrolling has been interrupted
+				    },
+				    x: false,
+				    y: true
+				}
+
+				var vueScroll = VueScrollTo.scrollTo(this.$refs.notice, 500, options)
+            }
+        }
+    }
+</script>
---
+++ grav/user/themes/haywire/js/components/Hero.vue
@@ -0,0 +1,30 @@
+<script>
+
+var VueScrollTo = require('vue-scrollto');
+
+    export default {
+        methods: {
+            scrollDown() {
+
+            	const height = this.$refs.section.offsetHeight
+
+            	var options = {
+				    container: 'body',
+				    easing: 'ease-in',
+				    offset: height,
+				    cancelable: true,
+				    onDone: function() {
+				      // scrolling is done
+				    },
+				    onCancel: function() {
+				      // scrolling has been interrupted
+				    },
+				    x: false,
+				    y: true
+				}
+
+				var vueScroll = VueScrollTo.scrollTo(this.$refs.section, 500, options)
+            }
+        }
+    }
+</script>
