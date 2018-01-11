0cbf55f9d3833d564f0aaa6ee6245acdc520b616
Austin Rivas <arivas@pushhere.com>
2018-01-11 11:44:27 -0500

    added GravConfig mixin

---
+++ grav/user/themes/haywire/js/components/mixins/GravConfig.vue
@@ -0,0 +1,21 @@
+<script>
+    export default {
+        methods: {
+            async getGravConfig(config) {
+                let _this = this,
+                    refs = Object.keys(config);
+                if (refs && refs.length) {
+                    return await refs.reduce(_this.getGravProperty, Object.assign({}, config));
+                } else {
+                    return config;
+                }
+            },
+            getGravProperty(accumulator, key) {
+                let _this = this,
+                    $ref = `GRAV${key}`;
+                accumulator[key] = _this.$refs[$ref] && _this.$refs[$ref].content ?  _this.$refs[$ref].content : null;
+                return accumulator;
+            }
+        }
+    }
+</script>
