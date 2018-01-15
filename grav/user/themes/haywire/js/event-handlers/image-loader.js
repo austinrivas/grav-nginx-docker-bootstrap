ab7ea0a22ece436f0eb3b3271e5f31e63aab96a7
Austin Rivas <arivas@pushhere.com>
2018-01-15 10:15:38 -0500

    added nodelist polyfill for ie

--- grav/user/themes/haywire/js/event-handlers/image-loader.js
+++ grav/user/themes/haywire/js/event-handlers/image-loader.js
@@ -1,5 +1,10 @@
 export default class ImageLoader {
     constructor() {
+        // NodeList polyfill for IE, still doing this in 2018
+        (function () {
+            if ( typeof NodeList.prototype.forEach === "function" ) return false;
+            NodeList.prototype.forEach = Array.prototype.forEach;
+        })();
         document.querySelectorAll('img').forEach((element) => {
             element.addEventListener('load', () => {
                 element.classList.add('loaded');
