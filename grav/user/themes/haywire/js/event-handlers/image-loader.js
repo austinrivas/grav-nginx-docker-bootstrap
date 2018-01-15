911ddfefed0702968ff6f024218d33c9dffb3706
Austin Rivas <arivas@pushhere.com>
2018-01-15 10:47:15 -0500

    moved polyfill into app.js

--- grav/user/themes/haywire/js/event-handlers/image-loader.js
+++ grav/user/themes/haywire/js/event-handlers/image-loader.js
@@ -1,10 +1,5 @@
 export default class ImageLoader {
     constructor() {
-        // NodeList polyfill for IE, still doing this in 2018
-        (function () {
-            if ( typeof NodeList.prototype.forEach === "function" ) return false;
-            NodeList.prototype.forEach = Array.prototype.forEach;
-        })();
         document.querySelectorAll('img').forEach((element) => {
             element.addEventListener('load', () => {
                 element.classList.add('loaded');
